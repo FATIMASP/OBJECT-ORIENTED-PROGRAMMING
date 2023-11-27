8ae6f0ee8c71d175b1629877cf92d4e24324ebc1
FATIMASP <107749724+FATIMASP@users.noreply.github.com>
2023-11-27 00:29:49 +0000

    Update config.py

--- YouTube/YouTube_Top_Videos/config.py
+++ YouTube/YouTube_Top_Videos/config.py
@@ -4,6 +4,5 @@ search_query = 'BUN KEBAB RECIPES'
 max_results = 1000000
 top = 5
 final_cols = ['SNo', 'Video Title', 'Views', 'Likes','Video Link', 'Channel Title', 'Channel Link', 'Subscribers', 'Channel Views', 'Total Videos']
-int_cols = ['Views', 'Likes', 'Channel Views', 'Total Videos']
 sorting_cols = ['Likes', 'Views']
-sorting_order = [False, False] # False will sort in Descending Order and number of list elements must be equal to those in sorting_cols
+sorting_order = [False, False] # False will sort in Descending Order where number of list elements must be equal to those in sorting_cols
