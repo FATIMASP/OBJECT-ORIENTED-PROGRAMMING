f080acc201c011cbbca89a7260a3f106c3b00a0a
FATIMASP <107749724+FATIMASP@users.noreply.github.com>
2023-11-27 00:25:43 +0000

    Update config.py

--- YouTube/YouTube_Top_Videos/config.py
+++ YouTube/YouTube_Top_Videos/config.py
@@ -6,4 +6,4 @@ top = 5
 final_cols = ['SNo', 'Video Title', 'Views', 'Likes','Video Link', 'Channel Title', 'Channel Link', 'Subscribers', 'Channel Views', 'Total Videos']
 int_cols = ['Views', 'Likes', 'Channel Views', 'Total Videos']
 sorting_cols = ['Likes', 'Views']
-sorting_order = [False, False]
+sorting_order = [False, False] # False will sort in Descending Order and number of list elements must be equal to those in sorting_cols
